09d6e93f597e408d584fa1f05fde0f53afba409a
thinkerpeng <664205468@qq.com>
2018-05-05 14:14:52 +0800

    修改ssmdemo包名为hrmapp

---
+++ src/main/java/com/pwx/hrmapp/exception/NoNumberException.java
@@ -0,0 +1,15 @@
+package com.pwx.hrmapp.exception;
+
+/**
+ * 库存不足异常
+ */
+public class NoNumberException extends RuntimeException {
+
+    public NoNumberException(String message){
+        super(message);
+    }
+
+    public NoNumberException(String message, Throwable cause){
+        super(message, cause);
+    }
+}
